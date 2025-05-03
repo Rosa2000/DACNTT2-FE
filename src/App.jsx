55a8e03ef978da034aa95c89c346516cf25dd31c
Rosa <73031904+Rosa2000@users.noreply.github.com>
2025-05-03 22:28:56 +0700

    create dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
-// src/App.js
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { AuthPage, ForgotPassword } from './pages/auth';
-import WelcomePage from './pages/welcome/WelcomePage';
-import Dashboard from './pages/dashboard/Dashboard';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// src/App.jsx
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { verifyLoginAsync } from './slices/authSlice';
+import AppRouter from './routes/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(verifyLoginAsync());
+  }, [dispatch]);
+
   return (
-    <GoogleOAuthProvider clientId="252976365448-nujo5mts3p67u715lif4drgjgsh3j7fl.apps.googleusercontent.com">
-    <div>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/auth/*" element={<AuthPage />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/" element={<Navigate to="/auth/login" replace />} />
-          <Route path="/grammar" element={<div>Trang Học Ngữ Pháp (Chưa triển khai)</div>} />
-          <Route path="/exercises" element={<div>Trang Bài Tập (Chưa triển khai)</div>} />
-          <Route path="/test" element={<div>Trang Kiểm Tra Trình Độ (Chưa triển khai)</div>} />
-          <Route path="/profile" element={<div>Trang Hồ Sơ (Chưa triển khai)</div>} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        <ToastContainer position="top-right" autoClose={3000} />
-    </div>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <div className="App">
+        <AppRouter />
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
 export default App;
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import AppRouter from './routes/AppRouter';
-// import { store } from './store';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <AppRouter />
-//     <ToastContainer />
-//   </Provider>,
-//   document.getElementById('root')
-// );
--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-          <App />
-        </GoogleOAuthProvider>
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId="252976365448-nujo5mts3p67u715lif4drgjgsh3j7fl.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </React.StrictMode>
 );
